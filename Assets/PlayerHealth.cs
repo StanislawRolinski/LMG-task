6a8f21aed334a0c0c3fab9235157c8e3fa4bdbf2
Rolinskyi <stanislav.rolinskyi@fujitsu.com>
2019-06-12 23:56:36 +0200

    Add player healht

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
