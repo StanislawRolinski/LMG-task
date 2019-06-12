e1fcafeda7c21d58c7f1ed2fad7a989b8bac4888
Rolinskyi <stanislav.rolinskyi@fujitsu.com>
2019-06-12 12:24:54 +0200

    Add Player, player animation, player movement, movement borders, Canvas

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 10f;
+
+    [SerializeField] Animator animator;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+        float moveX = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        float moveY = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+
+        transform.Translate(moveX, moveY, 0f);
+
+        animator.SetFloat("moveX", moveX);
+        animator.SetFloat("moveY", moveY);
+
+        if (Input.GetAxisRaw("Horizontal") == 1 ||
+            Input.GetAxisRaw("Horizontal") == -1 ||
+            Input.GetAxisRaw("Vertical") == 1 ||
+            Input.GetAxisRaw("Vertical") == -1)
+        {
+            animator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+    }
+}
